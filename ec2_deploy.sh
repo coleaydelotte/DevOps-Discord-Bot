8db0b3349952d2551cff2ffc4baeac20e7c463e6
Cole Aydelotte <aydelottec@moravian.edu>
2024-04-25 17:41:09 -0400

    Created local and ec2 scripts
    Created local and ec2 scripts

--- ec2_deploy.sh
+++ ec2_deploy.sh
@@ -1,10 +1,9 @@
-sudo yum install docker
-sudo yum install libxcrypt-compat
+sudo yum install -y docker
+sudo yum install -y libxcrypt-compat
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-cd DevOps-Discord-Bot/
+sudo systemctl start docker
 
 ./up
-
